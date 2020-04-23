a62d24029a7ba665bd0f44ac9d67bb74dd07ef0f
Samantha Wiley <samantha.wiley@student.csulb.edu>
2020-04-23 14:05:50 -0700

    Fetch urls from Variants API and Respond

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+const VARIANTSAPI = 'https://cfw-takehome.developers.workers.dev/api/variants'
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 /**
- * Respond with hello worker text
+ * Respond with response from given variant url
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  let url = await fetchVariantURL()
+
+  return await fetch(url)
 }
+/**
+ * Fetches and returns random variant URL from Variants API ('https://cfw-takehome.developers.workers.dev/api/variants')
+ * 
+ */
+async function fetchVariantURL() {
+  let response = await fetch(VARIANTSAPI)
+  let data = await response.json()
+  let variants = data.variants
+
+  return variants[(Math.random() < 0.5 ? 0 : 1)]
+}
+
